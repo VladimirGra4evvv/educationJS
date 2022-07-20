bf2acce18539eb5235882d91fed806752f62e1b1
Vladimir <gra4evvv@yandex.ru>
2022-07-20 16:58:08 +0300

    Выполнен 3 урок до задание 7

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*  
+confirm - вывод модального окна
+prompt - модальное окно для ввода значений
+
+
+*/
+
+'use strict';
+
 let money;
 let income;
 let addExpenses;
@@ -7,12 +16,19 @@ let period;
 let str = 'Hello my Friends!';
 let myArray;
 let budgetDay;
+let requiredExpenses1;
+let requiredExpenses2;
+let howMuch1;
+let howMuch2;
+let budgetMonth;
 
-money = 98000;
+mission = 460000;
+
+/*money = 98000;
 income = 'фриланс';
 addExpenses = 'Дети, Коммуналка, Спорт';
 deposit = true;
-mission = 460000;
+
 period = 6;
 
 
@@ -30,3 +46,31 @@ console.log(myArray);
 budgetDay = money/30;
 
 console.log('Бюджет в день ' + budgetDay.toFixed(2));
+*/
+
+
+money = Number(prompt('Ваш месячный доход?', 0));
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+deposit = prompt('Есть ли у вас депозит в банке?');
+if(deposit === 'да'){
+    deposit = true;
+} else {
+    deposit = false;
+}
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+requiredExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+howMuch1 = Number(prompt('Во сколько вам это обходится?'));
+requiredExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+howMuch2 = Number(prompt('Во сколько вам это обходится?'));
+
+budgetMonth = money - (howMuch1 + howMuch2);
+console.log(budgetMonth);
+
+mission = mission/budgetMonth;
+console.log('Цель будет достигнута за: ' + mission.toFixed(0) + ' мес');
